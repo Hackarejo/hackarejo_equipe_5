7e4b2bb4fd44563e99445214f80fefaa779c424e
Alan Diego Darold <alnhcl1@gmail.com>
2016-04-16 19:43:46 -0300

    add
    add app

---
+++ app_android/app/src/main/java/com/quebragelo/quebragelo/helper/WebAddressable.java
@@ -0,0 +1,11 @@
+package com.quebragelo.quebragelo.helper;
+
+/**
+ * Created by bruno on 13/06/15.
+ */
+public interface WebAddressable {
+
+    String getPath();
+    Boolean hasId();
+    int getId();
+}
